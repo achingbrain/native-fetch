cbe0f2589561ad8190e6ee07fc6664fa6d2aa454
Alex Potsides <alex@achingbrain.net>
2021-12-13 11:05:10 +0000

    chore: esm/cjs publishing (#5)
    Publish both esm and cjs.
    
    Also, default types to browser fetch.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-'use strict'
+const globalFetch = globalThis.fetch
+const globalHeaders = globalThis.Headers
+const globalRequest = globalThis.Request
+const globalResponse = globalThis.Response
 
-// @ts-expect-error globalThis.fetch is defined according to the env types but it's not in node
-if (globalThis.fetch && globalThis.Headers && globalThis.Request && globalThis.Response) {
-  module.exports = {
-    fetch: globalThis.fetch,
-    Headers: globalThis.Headers,
-    Request: globalThis.Request,
-    Response: globalThis.Response
-  }
-} else {
-  module.exports = {
-    fetch: require('undici').fetch,
-    Headers: require('undici').Headers,
-    Request: require('undici').Request,
-    Response: require('undici').Response
-  }
-}
+export { globalFetch as fetch }
+export { globalHeaders as Headers }
+export { globalRequest as Request }
+export { globalResponse as Response }
